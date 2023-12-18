a865d120d1821cdf265362eaf071162c31ca989c
dfaz3019 <126225312+dfaz3019@users.noreply.github.com>
2023-12-18 22:30:25 +1100

    Add files via upload
    recoded, made improvements, added CSS

--- adskipper.js
+++ adskipper.js
@@ -1,11 +1,23 @@
 let button = document.createElement("button");
+let buttonContainer = document.createElement("div");
+const skipButton = document.querySelector(".skip-button")
 
-setTimeout(function () {
-    const targetDiv = document.querySelector("#top-level-buttons-computed");
-    button.textContent = "Skip Ad";
-    targetDiv.appendChild(button);
-}, 3000)
+buttonContainer.className = "skip-button-container";
+button.className = "skip-button";
+button.textContent = "Skip Ad";
 
-button.addEventListener("click", function() {
+document.addEventListener('yt-navigate-start', function () {
+    setTimeout(() => {
+        const targetDiv = document.querySelector("#owner");
+        if (targetDiv) {
+            targetDiv.appendChild(buttonContainer);
+            buttonContainer.appendChild(button);
+        } else {
+            console.warn("DIV DOES NOT EXIST");
+        }
+    }, 1500);
+})
+
+skipButton.addEventListener('click', () => {
     document.querySelector("video").currentTime = document.querySelector("video").duration;
 })
