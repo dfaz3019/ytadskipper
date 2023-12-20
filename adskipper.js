ee238b4939e956cbcd0e52ef2be353918f8a0ea8
dfaz3019 <126225312+dfaz3019@users.noreply.github.com>
2023-12-20 17:24:22 +1100

    Fixed button generation + button event listener

--- adskipper.js
+++ adskipper.js
@@ -1,23 +1,26 @@
-let button = document.createElement("button");
-let buttonContainer = document.createElement("div");
-const skipButton = document.querySelector(".skip-button")
+const newButton = document.createElement("button");
+const newButtonContainer = document.createElement("div");
+const buttonContainer = document.querySelector(".skip-button-container");
+newButtonContainer.className = "skip-button-container";
+newButton.className = "skip-button";
+newButton.textContent = "Skip Ad";
 
-buttonContainer.className = "skip-button-container";
-button.className = "skip-button";
-button.textContent = "Skip Ad";
-
-document.addEventListener('yt-navigate-start', function () {
-    setTimeout(() => {
-        const targetDiv = document.querySelector("#owner");
-        if (targetDiv) {
-            targetDiv.appendChild(buttonContainer);
-            buttonContainer.appendChild(button);
-        } else {
-            console.warn("DIV DOES NOT EXIST");
-        }
-    }, 1500);
+document.addEventListener("yt-navigate-finish", () => {
+    if (buttonContainer) {
+        buttonContainer.remove();
+    }
+    const targetDiv = document.querySelector("#owner");
+    if (targetDiv) {
+        targetDiv.appendChild(newButtonContainer);
+        newButtonContainer.appendChild(newButton);
+        const skipButton = document.querySelector(".skip-button");
+        skipButton.addEventListener("click", skipAd);
+    } else {
+        console.warn("DIV DOES NOT EXIST");
+    }
 })
 
-skipButton.addEventListener('click', () => {
+const skipAd = () => {
     document.querySelector("video").currentTime = document.querySelector("video").duration;
-})
+    console.log("Event Listener Initialised!")
+}
